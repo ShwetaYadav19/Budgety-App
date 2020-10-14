73bd6055f8cb3cb855d8aba8b96d4e4d1b9480bd
Shweta Yadav <yshweta@amazon.com>
2020-10-14 09:38:16 +0530

    Adding DOM structure for input values

--- app.js
+++ app.js
@@ -7,14 +7,36 @@ var budgetController = (function () {
 //UI CONTROLLER
 var UIController = (function () {
 
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescritption: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,//It will be either inc or exp
+                description: document.querySelector(DOMStrings.inputDescritption).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+
+        getDOMStrings: function () {
+            return DOMStrings;
+        }
+    };
+
 })();
 
 //GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMStrings();
     var ctrlAddItem = function () {
         //1. GET INPUT FIELD DATA
-
+        var input = UICtrl.getInput();
+        console.log(input);
 
         //2. ADD ITEM TO BUDGET CONTROLLER
 
@@ -27,7 +49,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         console.log('It works');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
 
